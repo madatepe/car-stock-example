fa2e2bd92a30d2adc7f938b28f8e453c8f054b42
madatepe <mertadatepe0@gmail.com>
2023-05-13 06:06:55 +0300

    Updated Button component with new props

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,7 +3,8 @@ export default {
   name: 'Button',
   props: {
     text: { type: String, required: false, default: 'Action!' },
-    color: { type: String, required: false, default: '#3498db' },
+    cancel: { type: Boolean, required: false, default: false },
+    disabled: { type: Boolean, required: false, default: false },
   },
 }
 </script>
@@ -11,7 +12,11 @@ export default {
 <template>
   <button
     class="app-button"
-    :style="{ backgroundColor: color }"
+    :disabled="disabled"
+    :class="{
+      'app-button__cancel': cancel,
+      'app-button__disabled': disabled,
+    }"
     v-on="$listeners"
   >
     {{ text }}
@@ -20,6 +25,7 @@ export default {
 
 <style lang="scss">
 .app-button {
+  background: #3498db;
   border: none;
   color: #fff;
   padding: 0.5rem 1rem;
@@ -29,6 +35,15 @@ export default {
   &:hover {
     background: #2779b0;
     cursor: pointer;
+  }
+  
+  &__cancel {
+    background: #cf3131 !important;
+  }
+
+  &__disabled {
+    background: #a8a8a8 !important;
+    pointer-events: none;
   }
 }
 </style>
--- src/modules/EditPage/index.vue
+++ src/modules/EditPage/index.vue
@@ -10,16 +10,7 @@ export default {
   data() {
     return {
       car: null,
-      hpRangeErrorVisible: false,
-      colors: [
-        'red',
-        'blue',
-        'green',
-        'black',
-        'orange',
-        'purple',
-        'brown',
-      ],
+      colors: ['red', 'blue', 'green', 'black', 'orange', 'purple', 'brown'],
     };
   },
   created() {
@@ -33,12 +24,13 @@ export default {
       this.$emit('updateCar', this.car);
     },
   },
-  watch: {
-    'car.hp'(newVal) {
-      if (newVal < 100 || newVal > 550) {
-        this.hpRangeErrorVisible = true;
-      }
-    }
+  computed: {
+    hpRangeErrorVisible() {
+      return this.car.hp < 100 || this.car.hp > 550;
+    },
+    saveButtonDisable() {
+      return this.hpRangeErrorVisible || this.car === this.selectedCar;
+    },
   },
 }
 </script>
@@ -54,16 +46,16 @@ export default {
       </p>
       <p>
         <b>HORSE POWER:</b>
-        <input v-model.number="car.hp" />
+        <input v-model.number="car.hp" class="text-input" />
         <span v-show="hpRangeErrorVisible" class="error-text">
           * Please write value between 100 - 550
         </span>
       </p>
       <p>
         <b>PRICE:</b>
-        <input v-model.number="car.price" />
+        <input v-model.number="car.price" class="text-input" />
       </p>
-      <p><b>COLOR:</b></p>
+      <p><br><b>COLOR:</b></p>
       <div v-for="(color, index) in colors" :key="index">
         <input type="radio" :id="color" :value="color" v-model="car.color" />
         <label :for="color" :style="{ color: color }">
@@ -73,8 +65,8 @@ export default {
     </div>
 
     <div class="edit-page__actions">
-      <Button color="#cf3131" text="Cancel" @click="cancel" />
-      <Button text="Save" @click="save" />
+      <Button text="Cancel" @click="cancel" cancel />
+      <Button text="Save" @click="save" :disabled="saveButtonDisable" />
     </div>
   </div>
 </template>
@@ -83,8 +75,24 @@ export default {
 .edit-page {
   text-align: start;
 
+  input {
+    margin-left: 0.5rem;
+    min-width: 1rem;
+    margin-top: 0.5rem;
+  }
+  
   .error-text {
     color: #e00000
+  }
+  
+  .text-input {
+    display: flex;
+    font-size: 1.2rem;
+    margin: 0.5rem 0;
+    border-radius: 0.5rem;
+    color: #444;
+    padding: 0.5rem;
+    border: 1px solid #444;
   }
 
   &__actions {
